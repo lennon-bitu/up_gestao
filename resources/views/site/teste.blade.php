d7d2d08ac8d0950590cd815a655bf2cd94f77b12
Lennon Bitu <lennon.bitu@gmail.com>
2023-07-26 17:41:40 -0300

    Atualização view teste sintax blade
    atualização da view teste com algumas sintax de codigo blade
     foi adicionado exemplos de alguns codigo da sintax blade a esse arquivo teste.blade.php

--- resources/views/site/teste.blade.php
+++ resources/views/site/teste.blade.php
@@ -1,1 +1,17 @@
 <h3>{{ $nome }} <br /> {{ $categoria_id }} <br /> {{ $mensagem }}</h3>
+
+
+{{'chamada simpes de php com sintax blad'}}
+<?= 'chmada simples de php com a  teg da linguagem' ?>
+
+{{--comentario com a sintax do blade direto --}}
+
+{{--abertura de tag php para codigo puro dentro do blade--}}
+@php
+    //comentario de uma linha dentro da tag php
+
+    /*
+    //comentario de mutiplas linha dentro da tag php
+    */
+
+@endphp
