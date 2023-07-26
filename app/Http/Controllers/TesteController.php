152fda39ffd7c10ebf35e04680d4e6b367cbf5e1
Lennon Bitu <lennon.bitu@gmail.com>
2023-07-26 17:48:10 -0300

    Update TesteController.php

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -6,7 +6,8 @@ use Illuminate\Http\Request;
 
 class TesteController extends Controller
 {
-   public function teste(string $nome,int $categoria_id, string $mensagem = 'email não informado'){
-        return view('site.teste', compact('nome','categoria_id', 'mensagem'));
-   }
+    public function teste(string $nome, int $categoria_id, string $mensagem = 'email não informado')
+    {
+        return view('site.teste', compact('nome', 'categoria_id', 'mensagem'));
+    }
 }
